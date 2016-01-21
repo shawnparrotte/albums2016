34b75fc4b625467fab8e2111e66989726a0bcfdd
Shawn Parrotte <shawnparrotte@gmail.com>
2016-01-20 22:34:27 -0500

    fixing security

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express  = require("express");
 const app      = express();
 const mongoose = require("mongoose");
 const path     = require('path');
-
-var port = process.env.PORT || 3000;
+var port       = process.env.PORT || 3000;
 
 app.set("view engine", "ejs");
 
