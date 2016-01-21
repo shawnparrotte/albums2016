16857cccc7a01492aba2e146aedeb3fe2da49614
Shawn Parrotte <shawnparrotte@gmail.com>
2016-01-21 13:35:07 -0500

    adds form post to add albums

--- public/app.js
+++ public/app.js
@@ -1,30 +1,40 @@
-//create unique card with jquery
 
-var cardNumber = serverSide.length;
+//Make AJAX Request
 
-for(i = 0; i < cardNumber; i++){
-  var htmlInject = '<div class="container">';
-      htmlInject += '<div class="album-cover" style="background-image: url(' + serverSide[i].cover + ');"></div>';
-      htmlInject += '<div class="album-info">';
-      htmlInject += '<h2 class="artist">' + serverSide[i].artist + '</h2>';
-      htmlInject += '<h3 class="album-name">' + serverSide[i].album + '</h3>';
-      htmlInject += '<p class="genre">' + serverSide[i].genre + '</p>';
-      htmlInject += '<div class="footer">';
-      htmlInject += '<p class="score">' + serverSide[i].score + '</p>';
-      htmlInject += '<div class="listen"><a href="' + serverSide[i].link + '">Listen</a></div>';
-      htmlInject += '</div></div>';
+var goGetEm = $.getJSON("http://localhost:3000/api", function(result){})
 
-  $("#card-wrapper").append(htmlInject);
-}
+  .done(function(result){
 
-//interactivity
+    var cardNumber = result;
 
-$(".container").mouseenter(function(){
+    //Create Individual Cards
+    for(i = 0; i < cardNumber.length; i++){
+      var htmlInject = '<div class="container">';
+          htmlInject += '<div class="album-cover" style="background-image: url(' + cardNumber[i].cover + ');"></div>';
+          htmlInject += '<div class="album-info">';
+          htmlInject += '<h2 class="artist">' + cardNumber[i].artist + '</h2>';
+          htmlInject += '<h3 class="album-name">' + cardNumber[i].album + '</h3>';
+          htmlInject += '<p class="genre">' + cardNumber[i].genre + '</p>';
+          htmlInject += '<div class="footer">';
+          htmlInject += '<p class="score">' + cardNumber[i].score + '</p>';
+          htmlInject += '<div class="listen"><a href="' + cardNumber[i].link + '">Listen</a></div>';
+          htmlInject += '</div></div>';
+
+      //Append Card to Site
+      $("#card-wrapper").append(htmlInject);
+    }
+
+  });
+
+
+//Interactivity
+
+$( "#card-wrapper" ).on( "mouseenter", ".container", function() {
   var cont = $(this).children()
-
   $(cont[0]).css("background-size", "300px 300px");
-}).mouseleave(function(){
-  var cont = $(this).children()
+});
 
+$( "#card-wrapper" ).on( "mouseleave", ".container", function() {
+  var cont = $(this).children()
   $(cont[0]).css("background-size", "275px 275px");
-})
+});
--- server.js
+++ server.js
@@ -1,16 +1,40 @@
-const express  = require("express");
-const app      = express();
-const mongoose = require("mongoose");
-const path     = require('path');
-var port       = process.env.PORT || 3000;
+//Require Variables
+
+const express    = require("express");
+const app        = express();
+const mongoose   = require("mongoose");
+const bodyParser = require("body-parser");
+const path       = require('path');
+var port         = process.env.PORT || 3000;
+
+//Define Middleware
+
+var jsonParser = bodyParser.json();
+var urlParser = bodyParser.urlencoded({extended : false})
 
 app.set("view engine", "ejs");
-
 app.use(express.static("public"));
+
+//Connect to DB and Provide Schema
 
 mongoose.connect("mongodb://shawnp:F#A#infinity1@ds047345.mongolab.com:47345/albums2016");
 
 var db = mongoose.connection;
+
+var Schema = mongoose.Schema;
+
+var albumSchema = new Schema({
+  artist: String,
+  album: String,
+  genre: String,
+  score: String,
+  cover: String,
+  link: String
+});
+
+var Album = mongoose.model("Album", albumSchema);
+
+//Connect Port Once Database Loads
 
 db.on("error", console.error);
 
@@ -18,26 +42,41 @@ db.once("open", function(err){
   app.listen(port);
 });
 
-var schema = {
-  artist: String,
-  album: String,
-  genre: String,
-  score: String,
-  cover: String,
-  link: String
-}
-
-var Albums = mongoose.model("Albums", schema);
-var info;
-
-Albums.find(function(err, data){
-  if(err){
-    console.error;
-  } else {
-    info = JSON.stringify(data);
-  }
-});
+//App Routes
 
 app.get('/', function(req, res){
-  res.render("index", { information: info })
+  res.render("index");
 });
+
+app.get('/api', function(req, res){
+
+  //Load JSON Data from MongoDB
+  Album.find(function(err, data){
+    if(err){console.error}
+    else {res.json(data)}
+  });
+
+});
+
+app.get('/add-album', function(req, res){
+  res.render("add-album");
+});
+
+app.post('/add-album', urlParser, function(req, res){
+
+  var newAlbum = Album({
+    artist: req.body.artist,
+    album: req.body.album,
+    genre: req.body.genre,
+    score: req.body.score,
+    link: req.body.link,
+    cover: req.body.cover
+  })
+
+  newAlbum.save(function(err){
+    if (err) throw err;
+    console.log('album saved!');
+  })
+
+  res.render("add-album");
+})
